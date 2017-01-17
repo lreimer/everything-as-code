e9b4850fcca2c21257fbd4bf4f626d10b2e435ef
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2017-01-17 08:44:35 +0100

    Added Structurizr architecture definition.

--- src/test/groovy/everything/as/code/BooksArchitectureSpec.groovy
+++ src/test/groovy/everything/as/code/BooksArchitectureSpec.groovy
@@ -31,32 +31,47 @@ import com.structurizr.view.Shape
 import com.structurizr.view.Styles
 import spock.lang.Specification
 
-
 /**
  * Describe the Architecture-as-code using Structurizr.
  */
 class BooksArchitectureSpec extends Specification {
 
     def "Define the architecture using Structurizr"() {
-        expect:
+        expect: 'the architecture definition'
         def workspace = new Workspace("Everything-as-code", "This is a model of the system context of Everything-as-code.")
         def model = workspace.model
 
         // create a model and the software system we want to describe
-        def bookService = model.addSoftwareSystem("Book Service", "The best source to get info on books.")
+        def bookApp = model.addSoftwareSystem("Book Application", "The best source to get info on books.")
 
         // create the various types of people (roles) that use the software system
         def anonymousUser = model.addPerson("Anonymous User", "Anybody on the web.")
-        anonymousUser.uses(bookService, "Searches for books and views details.")
+        anonymousUser.uses(bookApp, "Searches for books and views details.")
+
+        def browser = bookApp.addContainer("Web Browser",
+                "Allows users to view information about books",
+                "Edge, Chrome, Firefox")
+        anonymousUser.uses(browser, "Views information from and makes requests to")
+
+        def webApp = bookApp.addContainer("Web Application",
+                "Hosts the browser-based web application and services",
+                "Payara Fish")
+        browser.uses(webApp, "uses [JSON/HTTPS]")
 
         // now create the system context view based upon the model
         def viewSet = workspace.views
-        def contextView = viewSet.createSystemContextView(bookService, "context", "The system context diagram for the book service.")
+        def contextView = viewSet.createSystemContextView(bookApp, "context", "The system context diagram for the book service.")
+        contextView.setPaperSize(PaperSize.Slide_4_3)
         contextView.addAllSoftwareSystems()
         contextView.addAllPeople()
 
+        def containerView = viewSet.createContainerView(bookApp, "container", "The container view diagram for the book service.")
+        containerView.setPaperSize(PaperSize.Slide_4_3)
+        containerView.addAllContainers()
+        containerView.addAllPeople()
+
         // tag and style some elements
-        bookService.addTags("Everything-as-code")
+        bookApp.addTags("Everything-as-code")
 
         Styles styles = viewSet.configuration.styles
         styles.addElementStyle(Tags.ELEMENT).width(600).height(450).color("#FFFFFF").fontSize(40).shape(Shape.RoundedBox)
@@ -64,9 +79,9 @@ class BooksArchitectureSpec extends Specification {
         styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#2A4E6E")
         styles.addElementStyle(Tags.PERSON).width(575).background("#728DA5").shape(Shape.Person)
         styles.addRelationshipStyle(Tags.RELATIONSHIP).thickness(5).fontSize(40).width(500)
-        contextView.setPaperSize(PaperSize.Slide_4_3)
 
-        def client = new StructurizrClient("4c41190b-fe56-40d3-97b0-c06687c1ca10", "6d28eecd-13ad-4292-ab31-c9ea762fb7d2")
+        // TODO: add Structurizr API credentials here
+        def client = new StructurizrClient("", "")
         client.putWorkspace(22571, workspace)
     }
 
